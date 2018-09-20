f44129dae6df1d030a82824df688e44c66c99109
Zan Javed <zan@Zans-MBP.attlocal.net>
2018-09-19 23:57:24 -0700

    added random color selection

--- colorGame.js
+++ colorGame.js
@@ -8,7 +8,7 @@ var colors = [
 ];
 
 var squares = document.querySelectorAll('.square');
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById('colorDisplay');
 var messageDisplay = document.querySelector('#message');
 colorDisplay.textContent = pickedColor;
@@ -37,5 +37,9 @@ function changeColors(color){
     for(var i = 0; i < colors.length; i++){
         squares[i].style.backgroundColor = color;
     }
+}
 
+function pickColor(){
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
 }
