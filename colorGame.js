ea412143ad0e47e5ae58b03a578e9c8e2bf130ab
Zan Javed <zjaved101@gmail.com>
2018-09-21 00:06:18 -0700

    reset button along with some cosmetic changes

--- colorGame.js
+++ colorGame.js
@@ -4,6 +4,22 @@ var squares = document.querySelectorAll('.square');
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById('colorDisplay');
 var messageDisplay = document.querySelector('#message');
+var h1 = document.querySelector('h1');
+var resetButton = document.querySelector('#reset');
+
+resetButton.addEventListener('click', function(){
+    colors = generateRandomColors(6);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    h1.style.backgroundColor = '#232323';
+    this.textContent = 'New Colors'
+
+
+    for(var i = 0; i < squares.length; i++){
+        squares[i].style.backgroundColor = colors[i];
+    }
+});
+
 colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++){
@@ -18,6 +34,8 @@ for(var i = 0; i < squares.length; i++){
         if(clickedColor === pickedColor){
             messageDisplay.textContent = 'Correct!';
             changeColors(clickedColor);
+            h1.style.backgroundColor = clickedColor;
+            resetButton.textContent = 'Play Again?'
         } else{
             this.style.background = '#232323';
             messageDisplay.textContent = 'Try Again';
